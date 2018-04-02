eb49021a52477e5bbcfc7b573ba9af22ca23f8e1
Mike Gregory <mgregory@mainstreetfamilycare.com>
2018-04-02 11:26:18 -0500

    adding tests

---
+++ RefactoringExample/AuthenticatorTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Xunit;
+
+namespace RefactoringExample
+{
+    public class AuthenticatorTests {
+        private Authenticator authenticator;
+        private TraditionalUser traditionalUser;
+        private ActiveDirectoryUser adUser;
+
+        public AuthenticatorTests() {
+            authenticator = new Authenticator();
+            traditionalUser = new TraditionalUser() { Password = "123!", Username = "testuser" };
+            adUser = new ActiveDirectoryUser() { Password = "abc!", Username = "testuser" };
+        }
+
+        [Theory]
+        [InlineData("123!", true)]
+        [InlineData("234!", false)]
+        public void TraditionalUser_Login_PassesAndFailsBasedOnPassword(string password, bool returnValue) {
+            var result = authenticator.Login(traditionalUser, password);
+            Assert.Equal(returnValue, result);
+        }
+
+        [Theory]
+        [InlineData("abc!", true)]
+        [InlineData("cde!", false)]
+        public void ActiveDirectoryUser_Login_PassesAndFailsBasedOnPassword(string password, bool returnValue) {
+            var result = authenticator.Login(adUser, password);
+            Assert.Equal(returnValue, result);
+        }
+    }
+}
