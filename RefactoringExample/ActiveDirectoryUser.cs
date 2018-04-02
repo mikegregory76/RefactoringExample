749c9bc1b4127ddf26c6ef5a19d24e5ee005d5d2
Mike Gregory <mgregory@mainstreetfamilycare.com>
2018-04-02 11:14:58 -0500

    base code

---
+++ RefactoringExample/ActiveDirectoryUser.cs
@@ -0,0 +1,10 @@
+﻿namespace RefactoringExample {
+    public class ActiveDirectoryUser {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public bool ValidatePassword(string inPassword) {
+            return inPassword == Password;
+        }
+    }
+}
---
+++ RefactoringExample/Authenticator.cs
@@ -0,0 +1,11 @@
+﻿namespace RefactoringExample {
+    public class Authenticator {
+        public bool Login(TraditionalUser user, string password) {
+            return user.ValidatePassword(password);
+        }
+
+        public bool Login(ActiveDirectoryUser user, string password) {
+            return user.ValidatePassword(password);
+        }
+    }
+}
